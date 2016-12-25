ac3a5e720566d8c70f6afeab724b7d97c85197b3
Lubov <lubovruzanova@gmail.com>
2016-12-25 19:11:32 +0300

    Add files via upload

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ProductsApp.Models;
+
+using System.Net;
+using System;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+     
+        
+        // по keyword возвращает обект типа Product , который я хочу передать клиенту
+          [HttpPost]
+        public Product PostProduct()
+        {
+            var text = Request.Content.ReadAsStringAsync();
+        
+            var product = ModelProducts.GetProductByKeyword(text.Result);
+            if (product == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return product;
+        }
+      
+
+        
+
+    }
+}
